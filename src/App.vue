762db8b8c208aa5ea22e43bcc16637c1f0b5a197
Manh Dev <manhgdev@gmail.com>
2026-06-27 16:53:40 +0700

    ok

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import MainPanel from '@/components/MainPanel.vue';
-</script>
-
-<template>
-  <main class="veo-panel-root bg-background text-foreground">
-    <MainPanel />
-  </main>
-</template>
--- src/composables/useControlResumeRegistration.js
+++
@@ -1,7 +0,0 @@
-import { onMounted, onUnmounted } from 'vue';
-import { registerResumeHandler, unregisterResumeHandler } from '@/composables/useActiveControlResume.js';
-
-export function useControlResumeRegistration(resumeHandler) {
-  onMounted(() => registerResumeHandler(resumeHandler));
-  onUnmounted(() => unregisterResumeHandler(resumeHandler));
-}
